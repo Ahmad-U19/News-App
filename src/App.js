39f3dd0521743895eb309337a313959f8db8cc69
Ahmad Hassaan <ahmedoppal2592@gmail.com>
2025-03-19 10:42:43 -0700

    Fragmenst

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component} from 'react'
 //import Navbar from './Components/Navbar';
 //import News from './Components/News';
-import LifeCycle from './Components/LifeCycle'
+//import LifeCycle from './Components/LifeCycle'
+import Fragment from './Components/Fragment'
 
 export default class App extends Component {
   render() {
     return (
      <div>
-      <LifeCycle />
-      {/* <Navbar />
-      <News /> */}
+      <Fragment />
      </div>
     )
   }
---
+++ src/Components/Fragment.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Fragment() {
+  return (
+    <React.Fragment>
+        <h1>
+            Fragment Demo
+        </h1>
+        <p>This is a Description of React Fragments</p>
+    </React.Fragment>
+)
+}
+
+export default Fragment
